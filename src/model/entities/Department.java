f7de46077d7b35f3c9ee104ea9461a741dc2dc8b
Luis Felipe <luisfelipedasilva039@gmail.com>
2025-12-14 15:24:45 -0300

    testando umas parada ai

---
+++ src/model/entities/Department.java
@@ -0,0 +1,23 @@
+package model.entities;
+
+public class Department {
+    private Integer id;
+    private String name;
+
+    public Department() {
+
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
